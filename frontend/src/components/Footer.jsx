e2bbbabcbf26aa69756ef45397384aeee6aecf0a
Danilo <sdann_26@hotmail.com>
2021-06-09 05:42:37 -0500

    Footer
    Agregando el footer y mejorando varios detalles

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 const Footer = () => {
     return (
-        <footer>
-            
+        <footer className="footer">
+            <p>Siguenos en nuestras Redes Sociales</p>
+            <div className="footer-social-icons">
+                <FacebookIcon fontSize="large" className="footer-social-icons__icon"/>
+                <TwitterIcon fontSize="large" className="footer-social-icons__icon"/>
+                <YouTubeIcon fontSize="large" className="footer-social-icons__icon"/>
+                <InstagramIcon fontSize="large" className="footer-social-icons__icon"/>
+            </div>
+            <p>&copy; 2021 ReadIt. Todos los derechos reservados.</p>
         </footer>
     )
 }
