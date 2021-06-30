4e09fdeb85340e624883503958e75aaeffe687d9
JesusATL <jtorrejonl@uni.pe>
2021-06-30 12:14:25 -0500

    Evitar baneos

--- frontend/src/components/BookReader.jsx
+++ frontend/src/components/BookReader.jsx
@@ -27,32 +27,6 @@ import { BorderAllRounded, Height } from "@material-ui/icons";
 
 const BookContent = (props) => {
   
-  const [constructorHasRun, setConstructorHasRun] = useState(false);
-
-  const constructor = () => {
-    if (constructorHasRun) return;
-    setConstructorHasRun(true);
-
-    console.log("--------------------Inline constructor()--------------------");
-    
-    (async () => {
-      var response = await fetch('https://www.gutenberg.org/files/58221/58221-h/58221-h.htm');
-      switch (response.status) {
-          // status "OK"
-          case 200:
-              var template = await response.text();
-  
-              console.log(template);
-              break;
-          // status "Not Found"
-          case 404:
-              console.log('Not Found');
-              break;
-      }
-  })();
-  };
-
-  constructor();
 
 
   const [starValue, setValue] = React.useState(3);
