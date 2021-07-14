e6f9c5626e1ca50b2bf83cf4546292df4ea6f1fc
Jordi Bardales <jbardalesr@uni.pe>
2021-07-14 12:15:35 -0500

    OK

--- frontend/src/services/LibroService.js
+++ frontend/src/services/LibroService.js
@@ -6,11 +6,16 @@ export const getLibros = async (user) => {
   return await axios.get(`${API}/libros`);
 };
 
-
 export const getLibro = async (id) => {
   return await axios.get(`${API}/libros/${id}`);
 };
 
+export const deleteAnotacion = async (idAnotacion, idLibro, idUsuario) => {
+  return await axios.put(`${API}/anotaciones/${idAnotacion}`, {
+    idLibro: idLibro,
+    idUsuario: idUsuario,
+  });
+};
 
 export const getAnotaciones = async (userId) => {
   return await axios.get(`${API}/anotaciones/${userId}`);
