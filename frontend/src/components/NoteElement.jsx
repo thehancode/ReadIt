04ec1cc851fe32770c3943b49f271fecf1ff7131
Danilo <sdann_26@hotmail.com>
2021-07-13 19:20:08 -0500

    Mis Notas

--- frontend/src/components/NoteElement.jsx
+++ frontend/src/components/NoteElement.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Speech from 'react-speech';
+import ReactTooltip from 'react-tooltip';
 
 import { makeStyles } from "@material-ui/core/styles";
 import IconButton from '@material-ui/core/IconButton';
@@ -22,24 +23,24 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: "0px 2px 2px 2px hsla(266, 23%, 20%, 0.3)",
   },
   noteTitle: {
-    width: "calc(40% - 100px)",
+    width: "calc(40% - 110px)",
   },
   noteTitle__p:{
     margin: "2px",
   },
   noteMessage: {
-    width: "calc(60% - 100px)",
+    width: "calc(60% - 110px)",
   },
   noteButtons: {
-    width: "200px",
-  },
-  noteButtons__speech: {
-    backgroundColor: 'blue',
+    width: "220px",
+    backgroundImage: "none",
   },
   noteButtons__icon: {
     backgroundColor: "#C0B3A0",
     color: "#21242b",
     margin: "0 8px",
+    backgroundImage: "none",
+    textTransform: "none",
     "&:hover": {
       backgroundColor: "hsl(36, 20%, 59%)",
     },
@@ -62,22 +63,36 @@ const NoteElement = (props) => {
         <p className={classes.noteTitle__p}>{note.comment}</p>
       </div>
       <div className={classes.noteButtons}>
-        <IconButton className={classes.noteButtons__icon} size="small" aria-label="here in the book">
+        <IconButton className={classes.noteButtons__icon} 
+          size="small" 
+          aria-label="here in the book" 
+          data-tip='' data-for='location' 
+          delay-show='500'          
+          href={"/read/"+note.id}
+        >
           <RoomIcon fontSize="large"/>
         </IconButton>
-        <Speech 
-          className={classes.noteButtons__speech}
-          textAsButton={true}    
-          displayText={
-          <IconButton className={classes.noteButtons__icon} size="small" aria-label="listen to message">
-            <PlayCircleOutlineIcon fontSize="large"/>
-          </IconButton >
-          }
-          text={note.comment}
-        />
-        <IconButton className={classes.noteButtons__icon} size="small" aria-label="delete note">
+        <ReactTooltip id="location" delayShow={500} backgroundColor="#C0B3A0" className="tooltip">
+          Redirigirte al libro
+        </ReactTooltip>
+        <IconButton className={classes.noteButtons__icon + " noteButtons__icon--size"} size="small" aria-label="listen to message" data-tip='' data-for='play' delay-show='500'>
+          <Speech
+            textAsButton={true}    
+            displayText={
+              <PlayCircleOutlineIcon className="noteButtons__icon--positicon" fontSize="large"/>
+            }
+            text={note.comment}
+          />
+        </IconButton >
+        <ReactTooltip id="play" delayShow={500} backgroundColor="#C0B3A0" className="tooltip">
+          Reproducir Mensaje
+        </ReactTooltip>
+        <IconButton className={classes.noteButtons__icon} size="small" aria-label="delete note" data-tip='' data-for='delete' delay-show='500'>
           <DeleteOutlineIcon fontSize="large"/>
         </IconButton>
+        <ReactTooltip id="delete" delayShow={500} backgroundColor="#C0B3A0" className="tooltip">
+          Eliminar Nota
+        </ReactTooltip>
       </div>      
     </div>
   );
